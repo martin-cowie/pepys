1da13ed9d4111b65e32dad081f093e576e604e63
Martin Cowie <treacleminer@gmail.com>
2022-11-16 15:28:35 +0000

    Logging of child-process output to the main process log mechanism.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pepys
 An ONVIF server for the Raspberry Pi Camera. Pronounced 'peeps'.
+
+## Requirements
+
+### Test Camdera Adapter
+
+* The Live555rtsp server. 
+
+To install on a Mac `brew install openrtsp`
+
+To install on Linux `sudo apt-get install livemedia-utils`
--- server/src/camera.rs
+++ server/src/camera.rs
@@ -1,13 +1,54 @@
-use tracing::info;
 use hyper::Uri;
+use std::error::Error;
+use std::process::{Stdio};
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::{Command, Child};
+use tracing::{info, warn};
 
 pub trait CameraAdapter: Send + Sync { // Extra traits so it can be shared with > 1 thread
     fn get_preview(&self) -> (Vec<u8>, String);
     fn get_camera_streams(&self) -> Vec<Uri>; //Uri does not handle RTSP URIs
 }
 
+
+/// Relay a pipe from a child process, by lines to a handler lambda
+macro_rules! relay_pipe_lines {
+    ($pipe:expr, $handler:expr) => {
+        tokio::spawn(async move {
+            let mut reader = BufReader::new($pipe).lines();
+
+            loop {
+                let line = reader
+                    .next_line()
+                    .await
+                    .unwrap_or_else(|_| Some(String::new()));
+
+                match line {
+                    None => break,
+                    Some(line) => $handler(line)
+                }
+
+            }
+        });
+    };
+}
+
+pub fn start_and_log_command(mut command: Command) -> Result<Child, Box<dyn Error>> {
+    command.stdout(Stdio::piped()).stderr(Stdio::piped());
+
+    let mut child = command.spawn()?;
+
+    let child_stdout = child.stdout.take().unwrap(); // remove `take` from here
+    let child_stderr = child.stderr.take().unwrap(); // .. or from here and it fails
+    let child_pid = child.id().unwrap();
+
+    relay_pipe_lines!(child_stdout, |line|info!("[pid {}:stdout]: {}", child_pid, line));
+    relay_pipe_lines!(child_stderr, |line|warn!("[pid {}:stderr]: {}", child_pid, line));
+
+    Ok(child)
+}
+
 //====| Test Implementation |=======================================
-use std::process::{Command, Child};
 use get_if_addrs::{get_if_addrs, IfAddr, Ifv4Addr};
 
 const RTSP_SERVER: &str = "live555MediaServer";
@@ -26,8 +67,8 @@ impl TestCameraAdapter {
         // let suffix = "sample.mkv";
         let stream_uris = get_stream_uris();
 
-        let child = Command::new(RTSP_SERVER).spawn().expect("Cannot start RTSP server");
-        info!("Started {} with pid {}", RTSP_SERVER, child.id());
+        let child = start_and_log_command(Command::new(RTSP_SERVER)).expect("Cannot start RTSP server");
+        info!("Started {} with pid {}", RTSP_SERVER, child.id().unwrap());
 
         Self {
             rtsp_server_process: child,
@@ -80,6 +121,8 @@ mod test {
         let uris = get_stream_uris();
 
         dbg!(uris);
+
+        //FIXME: make assertions
 
     }
 }
