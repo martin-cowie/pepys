b39f01ae6396819185e2e9279db8df96314dd9ab
Martin Cowie <mcowie@pushtechnology.com>
2022-11-18 09:48:07 +0000

    Make module into a single file, in line with other modules generated from XSD

--- xsd_rs/xop/src/lib.rs
+++ xsd_rs/xop/src/lib.rs
@@ -1,6 +1,19 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
 
-pub mod include;
-pub use include::*;
+pub mod include {
+    use validate::Validate;
+    use yaserde_derive::{YaDeserialize, YaSerialize};
 
-//TODO: make a single file, in line with other modules generated from XSD
+
+    // pub type Include = Include;
+    #[derive(Default, PartialEq, Debug, YaSerialize, YaDeserialize)]
+    #[yaserde(prefix = "tns", namespace = "tns: http://www.w3.org/2004/08/xop/include")]
+    pub struct Include {
+        #[yaserde(attribute, rename = "href")]
+        pub href: String,
+    }
+
+    impl Validate for Include {}
+
+}
+pub use include::*;
