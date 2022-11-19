7504edbe553aeef55e3e4b18ff5295e0f6193e80
Martin Cowie <mcowie@pushtechnology.com>
2022-11-19 12:15:57 +0000

    Allow envelope type selection.

--- build_scripts/xml-to-rs/README.md
+++ build_scripts/xml-to-rs/README.md
@@ -1,6 +1,9 @@
 # README
 
 This crate generates Rust source from input XML. Generated Rust sources
-are mostly generate canned 'known' responses for testing.
+are mostly generate canned 'known' responses for testing. Most instances
+of the `ExampleData` trait start this way.
 
-It uses crates in /wsdl_rs and /xsd_rs for the heavy lifting.
+It uses crates in /wsdl_rs and /xsd_rs for the heavy lifting to generate
+Rust source which is imperfect but useful, often requiring manual editing
+to match the intended Rust struct.
--- build_scripts/xml-to-rs/src/main.rs
+++ build_scripts/xml-to-rs/src/main.rs
@@ -1,11 +1,35 @@
 use std::io;
+use std::error::Error;
 
 /// Generate Rust source from input XML
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
 
-    //TODO: Allow selection of the envelope type.
-    let envelope: event::response::Envelope = yaserde::de::from_reader(io::stdin())
-        .expect("Cannot parse input");
+    let mut args = std::env::args();
+    let argv0 = args.next().expect("Cannot get argv[0]");
 
-    println!("{:#?}", envelope);
+    let package_name = args
+        .next()
+        .unwrap_or_else(||panic!("wrong # args, try {argv0} (event|devicemgmt|media) < some-xml"));
+
+    match package_name.as_ref() {
+        "event" => {
+            let envelope: event::response::Envelope = yaserde::de::from_reader(io::stdin())?;
+            println!("{:#?}", envelope);
+        }
+        "devicemgmt" => {
+            let envelope: devicemgmt::response::Envelope = yaserde::de::from_reader(io::stdin())?;
+            println!("{:#?}", envelope);
+        }
+        "media" => {
+            let envelope: media::response::Envelope = yaserde::de::from_reader(io::stdin())?;
+            println!("{:#?}", envelope);
+        }
+
+        _ => {
+            panic!("Unexpected envelope type '{package_name}', try {argv0} (event|devicemgmt|media) < some-xml");
+        }
+    };
+
+    Ok(())
+
 }
